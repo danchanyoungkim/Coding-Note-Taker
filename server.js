80713258d58266e26f7fd4ea172f79f056e22b18
Dan Kim <danchanyoungkim@gmail.com>
2022-03-20 00:26:16 -0400

    add server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const PORT = 3001;
+
+const app = express();
+
+// Middleware for parsing JSON and urlencoded data.
+app.use(express.json());
+app.use(express.urlencoded({ extended : true }));
+
+app.use(express.static('public'));
+
+// Routes
