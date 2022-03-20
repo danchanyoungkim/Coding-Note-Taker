a5800af42e7186c673346f62509bccddff0db3c2
Dan Kim <danchanyoungkim@gmail.com>
2022-03-20 05:44:48 -0400

    final server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
-// const fs = require('fs');
+const fs = require('fs');
 
 const apiRouter = require('./routes/api');
-const 
+const htmlRouter = require('./routes/api');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -14,7 +14,8 @@ app.use(express.urlencoded({ extended : true }));
 
 app.use(express.static('public'));
 
-// Routes
+app.use('/', htmlRouter); 
+app.use('/api', apiRouter);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
